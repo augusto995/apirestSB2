065150446c00f35863a51f9712a3086487df9c63
Augusto Ana <augustora95@gmail.com>
2024-09-30 21:44:31 -0300

    commit - video 11

--- src/main/java/com/example/inicial1/entities/Domicilio.java
+++ src/main/java/com/example/inicial1/entities/Domicilio.java
@@ -1,22 +1,25 @@
 package com.example.inicial1.entities;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 import lombok.*;
+import org.hibernate.envers.Audited;
 
 @Entity
+@Table(name = "domicilio")
 @AllArgsConstructor
 @NoArgsConstructor
 @Setter
 @Getter
-@ToString
-@Builder
-public class Domicilio {
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
+@Audited
+public class Domicilio extends Base{
+
+    @Column(name = "calle")
     private String calle;
+
+    @Column(name = "numero")
     private int numero;
+
+    @ManyToOne(optional = false)
+    @JoinColumn(name = "fk_localidad")
+    private Localidad localidad;
 }
--- src/main/java/com/example/inicial1/entities/Persona.java
+++ src/main/java/com/example/inicial1/entities/Persona.java
@@ -5,7 +5,8 @@ import lombok.*;
 import lombok.experimental.SuperBuilder;
 import org.hibernate.envers.Audited;
 
-import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
 @AllArgsConstructor
@@ -24,9 +25,18 @@ public class Persona extends Base {
     private String apellido;
     @Column(name = "dni")
     private int dni;
-/*    @OneToOne(cascade = CascadeType.ALL)
+
+    @OneToOne(cascade = CascadeType.ALL)
     @JoinColumn(name = "fk_domicilio")
     private Domicilio domicilio;
-    */
+
+    @OneToMany(cascade = CascadeType.ALL, orphanRemoval = true)
+    @JoinTable(
+            name = "persona_libro",
+            joinColumns = @JoinColumn(name = "persona_id"),
+            inverseJoinColumns = @JoinColumn(name = "libro_id")
+    )
+
+    private List<Libro> libros = new ArrayList<Libro>();
 }
 
